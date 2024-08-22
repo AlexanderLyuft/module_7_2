593588ac16af488ba5f1839a74e102d020af4876
AlexanderLyuft <alexanderlyuft@mail.ru>
2024-08-22 22:05:15 +0700

    Add files via upload

---
+++ module_7_2.py
@@ -0,0 +1,29 @@
+        # Домашнее задание по теме "Позиционирование в файле".
+
+
+
+def custom_write(file_name, strings):
+    file = open(file_name, 'w', encoding='UTF-8')
+    strings_position = {}
+    for line in range(0, len(strings)):
+        strings_position[(line+1, file.tell())] = strings[line]
+        file.write(str(strings[line]) + '\n')
+    file.close()
+    return strings_position
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо Бро!'
+]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
+
+
+
+
+
+
